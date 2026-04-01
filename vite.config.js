534884740651b27d353590bc0e38f25567ec2c63
blakenreed <blake@salesoptima.com>
2026-04-01 18:42:08 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [react(), viteSingleFile()],
   build: {
     target: 'esnext',
     minify: 'esbuild',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  preview: {
-    port: 4173,
   },
 })
